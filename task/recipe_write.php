77c67cbf783b1d2731d44ac16345b45073638829
澁谷海渡 <sivukai.rice@gmail.com>
2015-06-06 14:40:34 +0900

    spec5: IDを割り振り，そのファイルを読み込んで表示する機能を実装

--- task/recipe_write.php
+++ task/recipe_write.php
@@ -3,11 +3,24 @@
 $filename = 'recipe-data.txt';
 
 // ファイルを書き込み可能状態で開く
-$handle = fopen($filename, 'w');
+$handle_write = fopen($filename, 'w');
+$array_menu = array("オムライス","杏仁豆腐","親子丼");
 
-// ファイルに"オムライス"という文字列を書き込む
-fwrite($handle, "オムライス");
-
+for($i=0; $i < count($array_menu);$i++){
+	// ファイルにメニュー内の文字列を１行ずつ書き込む
+	fwrite($handle_write, ($i+1).",".$array_menu[$i]."\n");
+}
 // ファイルの操作を終了する
-fclose($handle);
+fclose($handle_write);
+
+// ファイルを読み込み状態で開く
+$handle_load = fopen($filename, 'r');
+
+	while (!feof($handle_load)) {
+		//ファイルから１行取得
+		$recipe_name = fgets($handle_load);
+		echo $recipe_name; //出力
+	}
+// ファイルの操作を終了する
+fclose($handle_load);
 ?>
