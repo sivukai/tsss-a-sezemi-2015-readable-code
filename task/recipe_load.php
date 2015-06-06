f7d2bded1962c866fbd5f64d1168213bf11daa44
澁谷海渡 <sivukai.rice@gmail.com>
2015-06-06 14:22:54 +0900

    spec4:ファイルから複数行のデータを読み込む機能を実装

--- task/recipe_load.php
+++ task/recipe_load.php
@@ -5,8 +5,10 @@ $filename = 'recipe-data.txt';
 // ファイルを読み込み状態で開く
 $handle = fopen($filename, 'r');
 
-//ファイルから１行取得
-$recipe_name = fgets($handle);
+while (!feof($handle)) {
+	//ファイルから１行取得
+	$recipe_name = fgets($handle);
+	echo $recipe_name; //出力
+}
 
-echo $recipe_name."\n"; //出力
 ?>
